1bbe29d95f3532cf2def1198b94d8f11559651f6
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-12-29 23:31:56 +0100

    init

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,45 @@
 //! # Examples
 //!
 //! ```
-//! // tbi
+//! for [n1, n2, n3] in (1..5).combinations() {
+//!     println!("{}, {}, {}", n1, n2, n3);
+//! }
 //! ```
 
-#![forbid(unsafe_code, rust_2018_idioms)]
-#![deny(missing_debug_implementations, nonstandard_style)]
-#![warn(missing_docs, future_incompatible, unreachable_pub)]
+#![feature(min_const_generics)]
+
+use std::iter::Iterator;
+
+trait IterExt: std::iter::Iterator + Sized {
+    fn combinations<const N: usize>(&self) -> Combinations<'_, Self::Item, N> {
+        Combinations::new(self)
+    }
+}
+
+impl<T> IterExt for T where T: std::iter::Iterator {}
+
+pub struct Combinations<'a, T, const N: usize> {
+    iter: &'a dyn Iterator<Item = T>,
+    current_n: usize,
+    current_index: usize,
+    indexes: [usize; N],
+}
+
+impl<'a, T, const N: usize> Combinations<'a, T, N> {
+    fn new(iter: &'a dyn Iterator<Item = T>) -> Self {
+        Self {
+            iter,
+            current_n: 0,
+            current_index: 0,
+            indexes: [0; N],
+        }
+    }
+}
+
+impl<'a, T, const N: usize> Iterator for Combinations<'a, T, N> {
+    type Item = &'a [T; N];
+
+    fn next(&mut self) -> Option<&'a [T; N]> {
+        todo!();
+    }
+}
