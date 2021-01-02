1486533ee6562419b162ba4832cfb7643ddfd4b4
Jakub Valtar <jakub.valtar@gmail.com>
2021-01-02 02:33:25 +0100

    Added permutation order test

--- src/permutations.rs
+++ src/permutations.rs
@@ -112,6 +112,19 @@ mod test {
     use super::FullPermutations;
 
     #[test]
+    fn order() {
+        let mut permutations = (1..4).permutations();
+        assert_eq!(permutations.next(), Some([1, 2]));
+        assert_eq!(permutations.next(), Some([2, 1]));
+        assert_eq!(permutations.next(), Some([1, 3]));
+        assert_eq!(permutations.next(), Some([3, 1]));
+        assert_eq!(permutations.next(), Some([2, 3]));
+        assert_eq!(permutations.next(), Some([3, 2]));
+        assert_eq!(permutations.next(), None);
+        assert_eq!(permutations.next(), None);
+    }
+
+    #[test]
     fn full_order() {
         let mut permutations = FullPermutations::new([1, 2, 3]);
         assert_eq!(permutations.next(), Some([1, 2, 3]));
